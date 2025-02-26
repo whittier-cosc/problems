bd0851e4b4fbe0b5dd576172249a3a3dd8a40d9d
Jeff Lutgen <jlutgen@gmail.com>
2025-02-25 16:31:55 -0800

    raw string fix

--- encrypt/__init__.py
+++ encrypt/__init__.py
@@ -14,27 +14,27 @@ def compiles():
 @check50.check(compiles)
 def encrypts_a_as_b():
     """encrypts "a" as "b" using 1 as key"""
-    check50.run("./encrypt 1").stdin("a").stdout("ciphertext:\s*b\n", "ciphertext: b\n").exit(0)
+    check50.run("./encrypt 1").stdin("a").stdout(r"ciphertext:\s*b\n", "ciphertext: b\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_barfoo_as_yxocll():
     """encrypts "barfoo" as "yxocll" using 23 as key"""
-    check50.run("./encrypt 23").stdin("barfoo").stdout("ciphertext:\s*yxocll\n", "ciphertext: yxocll\n").exit(0)
+    check50.run("./encrypt 23").stdin("barfoo").stdout(r"ciphertext:\s*yxocll\n", "ciphertext: yxocll\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_BARFOO_as_EDUIRR():
     """encrypts "BARFOO" as "EDUIRR" using 3 as key"""
-    check50.run("./encrypt 3").stdin("BARFOO").stdout("ciphertext:\s*EDUIRR\n", "ciphertext: EDUIRR\n").exit(0)
+    check50.run("./encrypt 3").stdin("BARFOO").stdout(r"ciphertext:\s*EDUIRR\n", "ciphertext: EDUIRR\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_BaRFoo_FeVJss():
     """encrypts "BaRFoo" as "FeVJss" using 4 as key"""
-    check50.run("./encrypt 4").stdin("BaRFoo").stdout("ciphertext:\s*FeVJss\n", "ciphertext: FeVJss\n").exit(0)
+    check50.run("./encrypt 4").stdin("BaRFoo").stdout(r"ciphertext:\s*FeVJss\n", "ciphertext: FeVJss\n").exit(0)
 
 @check50.check(compiles)
 def checks_for_handling_non_alpha():
     """encrypts "world, say hello!" as "iadxp, emk tqxxa!" using 12 as key"""
-    check50.run("./encrypt 12").stdin("world, say hello!").stdout("ciphertext:\s*iadxp, emk tqxxa!\n", "ciphertext: iadxp, emk tqxxa!\n").exit(0)
+    check50.run("./encrypt 12").stdin("world, say hello!").stdout(r"ciphertext:\s*iadxp, emk tqxxa!\n", "ciphertext: iadxp, emk tqxxa!\n").exit(0)
 
 @check50.check(compiles)
 def handles_no_arg():
